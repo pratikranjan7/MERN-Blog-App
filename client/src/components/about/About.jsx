3d14a42380507166e5903d9418950ac9a14dac27
Pratik Ranjan <pratikranjan4597@gmail.com>
2022-08-29 01:17:43 +0530

    initial commit

---
+++ client/src/components/about/About.jsx
@@ -0,0 +1,55 @@
+
+import { Box, styled, Typography, Link } from '@mui/material';
+import { GitHub, Instagram, Email } from '@mui/icons-material';
+
+const Banner = styled(Box)`
+    background-image: url(https://i.imgur.com/JR8ilHf.jpg);
+    width: 100%;
+    height: 42vh;
+    background-position: left 0px top -250px;
+    background-size: cover;
+`;
+
+const Wrapper = styled(Box)`
+    padding: 20px;
+    & > h3, & > h5 {
+        margin-top: 50px;
+    }
+`;
+
+const Text = styled(Typography)`
+    color: #878787;
+`;
+
+const About = () => {
+
+    return (
+        <Box>
+            <Banner/>
+            <Wrapper>
+                <Typography variant="h3">Blogger by Pratik Ranjan</Typography>
+                <Text variant="h5">The best ideas can change who we are. Blogger is where those ideas take shape, take off, and spark powerful conversations. We’re an open platform where over 100 million readers come to find insightful and dynamic thinking. Here, expert and undiscovered voices alike dive into the heart of any topic and bring new ideas to the surface. Our purpose is to spread these ideas and deepen understanding of the world. We’re creating a new model for digital publishing. One that supports nuance, complexity, and vital storytelling without giving in to the incentives of advertising. It’s an environment that’s open to everyone but promotes substance and authenticity. And it’s where deeper connections forged between readers and writers can lead to discovery and growth. Together with millions of collaborators, we’re building a trusted and vibrant ecosystem fueled by important ideas and the people who think about them.<br></br>
+                <br></br>
+                If you are interested, you can view some of my favorite projects here
+                    <Box component="span" style={{ marginLeft: 5 }}>
+                        <Link href="https://github.com/pratikranjan7" color="inherit" target="_blank"><GitHub /></Link>
+                    </Box>
+                </Text>
+                <Text variant="h5">
+                    Need something built or simply want to have chat? Reach out to me on
+                    <Box component="span" style={{ marginLeft: 5 }}>
+                        <Link href="https://www.instagram.com/sample/" color="inherit" target="_blank">
+                            <Instagram />
+                        </Link>
+                    </Box>  
+                        or send me an Email 
+                        <Link href="mailto:pratikranjan4597@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
+                            <Email />
+                        </Link>.
+                </Text>
+            </Wrapper>
+        </Box>
+    )
+}
+
+export default About;
---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+//components
+import Connection from "./database/db.js";
+import Router from "./routes/route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", Router);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+const PORT = process.env.PORT || 8000;
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+const URL =
+  process.env.MONGODB_URI ||
+  `mongodb://${USERNAME}:${PASSWORD}@ac-um0wzlb-shard-00-00.fztoest.mongodb.net:27017,ac-um0wzlb-shard-00-01.fztoest.mongodb.net:27017,ac-um0wzlb-shard-00-02.fztoest.mongodb.net:27017/?ssl=true&replicaSet=atlas-hosrsg-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+Connection(URL);
+
+app.listen(PORT, () =>
+  console.log(`Server is running successfully on PORT ${PORT}`)
+);
